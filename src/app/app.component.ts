a3eea2b0eafc0147ee3f6079c452243af4932c2f
Joe Skeen <Joe Skeen>
2017-03-28 06:51:08 -0600

    display votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  color = 'pink';
+  colors = [
+    'pink',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'black',
+    'white',
+    'lightgray',
+    'brown',
+    'steelblue',
+    'springgreen',
+    'darkgoldenrod',
+    'blueviolet',
+    'cadetblue',
+    'antiquewhite',
+    'cyan',
+    'deeppink'
+  ];
+  votes: { [color: string]: number } = { };
+
+  voteForColor(color: string) {
+    this.color = color;    
+    this.votes[color] = (this.votes[color] || 0) + 1;
+    this.colors = this.colors.sort((color1, color2) => {
+      const votes1 = this.votes[color1] || 0;
+      const votes2 = this.votes[color2] || 0;
+      if (votes1 > votes2)
+        return -1;
+      
+      if (votes2 > votes1)
+        return 1;
+      
+      return 0;
+    });
+  }
+
 }
